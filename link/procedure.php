0f4636e570f9571f3712e37301eea95e1713bd35
Preeti240 <63953096+Preeti240@users.noreply.github.com>
2021-12-29 15:45:44 +0530

    Add files via upload

---
+++ link/procedure.php
@@ -0,0 +1,158 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="Why Recruit at NIT Silchar">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Placement Procedure</title>
+
+<!--links and scripts-->
+<link rel="icon" type="image/png" href="includes/images/vit-logo.png">
+
+<link rel="stylesheet" href="includes/css/bootstrap.min.css">
+<script src="includes/js/jquery.min.js"></script>
+<script src="includes/js/bootstrap.min.js"></script>
+<script src="includes/js/dropdown.js">  </script>
+<link rel="stylesheet" type="text/css" href="includes/css/style.css" />
+<!--page styles-->
+<style>
+	.main-content{
+		font-family:DroidSans, Arial, Helvetica, Rosario;
+		font-size:1.12em;
+		line-height:1.6em;
+	}
+	div.main-content h3{
+		padding:0;
+		background:none;
+		color:#010101;
+		text-align:left;
+		margin:1.1em 0.5em 0em 1.1em;
+		font-family:'Jura';
+	}
+	h2.custom{
+		margin-bottom:0;
+		font-size:2em;
+		color:rgba(48, 50, 210, 0.83);
+	}
+	h2+hr{
+		margin-top:0.2em;
+	}
+	h3+p{
+		font-size:0.9em;
+		padding:0.5em 1em;
+		background:#eaeaea;
+		margin:0.5em 2em;
+		/*-webkit-transition-duration:0.25s;
+		-moz-transition-duration:0.25s;
+		-o-transition-duration:0.25s;
+		-ms-transition-duration:0.25s;
+		transition-duration:0.25s;*/
+	}
+	/*h3+p:hover{
+		-webkit-transform:scale(1.03);
+		-moz-transform:scale(1.03);
+		-o-transform:scale(1.03);
+		-ms-transform:scale(1.03);
+		transform:scale(1.03);
+		background:#efefef;
+	}*/
+	@media only screen and(max-width:480px)
+	{
+		h3+p{
+		padding:0.5em 0.5em;
+		margin:0;
+		}
+	}
+</style>
+<!--end of page styles-->
+</head>
+<body>
+
+<!--	<hr class="header-sep" style="padding-top:0; margin-top:0;">
+
+	<hr class="header-sep">
+
+		<nav class="navbar navbar-inverse" role="navigation" style="clear:both;">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button> -->
+		  <!-- <a class="navbar-brand" href="#">Home</a> -->
+		<!--</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+		  <ul class="nav navbar-nav">
+			<li><a href="index">Home</a></li>
+			<li class="dropdown" id="placement">
+			  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Placement<span class="caret"></span></a>
+			  <ul class="dropdown-menu placementsub">
+				<li class="submenu"> <a href="placement-procedure">Procedure</a></li>
+
+				<li class="submenu"><a href="esteemed-recruiters">Recruiters</a></li>
+				<li class="submenu"><a href="includes/files/jnf_nits.docx">Job Notification Form</a></li>
+			  </ul>
+			</li>
+			<li class="dropdown" id="training">
+			  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Training<span class="caret"></span></a>
+			  <ul class="dropdown-menu trainingsub">
+				<li class="submenu"> <a href="internship-policies">Internship Policies</a></li>
+			  </ul>
+			</li>
+			<li class="dropdown" id="academic">
+			  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Academics<span class="caret"></span></a>
+			  <ul class="dropdown-menu academicsub">
+				<li class="submenu"> <a href="academic-life">Academic Life</a></li>
+				<li class="submenu"><a href="student-profile">Student Profile</a></li>
+			  </ul>
+			</li>
+			<li class="active"><a href="why-nits">Why recruit us</a></li>
+			<li><a href="team"> Team </a> </li>
+			<li><a href="facilities"> Facilities </a> </li>
+			<li><a href="reach-us"> Reach us </a> </li>
+		  </ul>
+		 </div>
+		</div>
+	</nav> -->
+
+
+
+
+
+<!-- main content of body here-->
+<div class="col-md-12" role=main>
+	<h2 class="custom" style="margin:0;"><b>PLACEMENT PROCEDURE</b></h2><hr class="stylish-rule" style="height:2.6px;">
+	<div class="main-content">
+		<h3></b>Pre-Placement Presentation</h3></b>
+			<p>This is the first part of the procedure. In the pre-placement presentation, the companies that have come for the campus placements put up presentations regarding their organisation. You get to know all about the companies, their recruitment process and the salaries you can expect. After the presentations, students are allowed to ask questions to the representatives.</p>
+
+			<h3></b>Qualifications</h3></b>
+			<p>In this step, you will know about the qualifications and other important criteria companies have set for recruitment. Usually, companies interview only those candidates that belong to a certain stream of education and will choose from among them. Since colleges teach multiple disciplines, and placements drives for all disciplines are held together, make sure you know which companies are interested in your education.</p>
+
+		  <h3></b>Written Examination</h3></b>
+			<p>Once you know the companies you are going to be applying for, you will have to attempt an aptitude test. The test may vary based on the positions you are applying for, but there are a few tests specific to education streams. For example, during engineering placement drives, the Wheebox Graduate Employability Test is the standard test that most companies use. This exam tests coding skills, math skills, English language skills, and critical thinking skills. Similarly, there are other tests for other disciplines.</p>
+
+		<h3></b>Group Discussion</h3></b>
+			<p>A few times, this round is not conducted, but many companies use this for filtering candidates. A group of students is created that is moderated by a judge. A common topic is given for the group to discuss. Once the discussions begin, students are judged on their knowledge, their communication skills, their confidence, their leadership skills, and their listening and retaining capabilities. In the GD round, the latest news and current affairs are discussed, so you need to brush up on your knowledge.</p>
+
+		<h3><b>Technical Knowledge Interview</h3></b>
+		<p>
+      This is an interview that will check your knowledge related to the subject you are studying, and other important technical knowledge that will be related to your profession. This round is mostly conducted in a one-on-one setting, but if there are many students and interviews are short on time, they may conduct interviews as a group.
+      </p>
+
+		<h3></b>Formal Interview</h3></b>
+			<p>This is the final part of the procedure – formal interview. In this round, you are judged for your confidence and abilities. You can even term this interview as your job interview. If you have done internships, or have worked on real industry projects, it will really help you do well during this interview. In this interview, you will most likely know if you have been selected.
+      </p>
+
+		<h3></b>Post Placement Discussion</h3></b>
+				<p>If you have cleared the formal interview, you will get an offer letter and a post-placement discussion. You will be given guidelines and be told about the joining procedure. You will also be told any other important information you need to know.</p>
+
+	</div>
+</div>
+<!--end of content-->
+
+
+
+</body>
+</html>
