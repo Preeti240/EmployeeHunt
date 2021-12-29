781e432be445c00018c2f1ffb861adee54ce64e4
Preeti240 <63953096+Preeti240@users.noreply.github.com>
2021-12-29 15:45:26 +0530

    Create why.php

---
+++ link/why.php
@@ -0,0 +1,159 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="Why Recruit at NIT Silchar">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Why US ?</title>
+
+<!--links and scripts-->
+<link rel="icon" type="image/png" href="includes/images/vit-logo.png">
+
+<link rel="stylesheet" href="includes/css/bootstrap.min.css">
+<script src="includes/js/jquery.min.js"></script>
+<script src="includes/js/bootstrap.min.js"></script>
+<script src="includes/js/dropdown.js">  </script>
+<link rel="stylesheet" type="text/css" href="includes/css/style.css" />
+<!--page styles-->
+<style>
+	.main-content{
+		font-family:DroidSans, Arial, Helvetica, Rosario;
+		font-size:1.12em;
+		line-height:1.6em;
+	}
+	div.main-content h3{
+		padding:0;
+		background:none;
+		color:#010101;
+		text-align:left;
+		margin:1.1em 0.5em 0em 1.1em;
+		font-family:'Jura';
+	}
+	h2.custom{
+		margin-bottom:0;
+		font-size:2em;
+		color:rgba(48, 50, 210, 0.83);
+	}
+	h2+hr{
+		margin-top:0.2em;
+	}
+	h3+p{
+		font-size:0.9em;
+		padding:0.5em 1em;
+		background:#eaeaea;
+		margin:0.5em 2em;
+		/*-webkit-transition-duration:0.25s;
+		-moz-transition-duration:0.25s;
+		-o-transition-duration:0.25s;
+		-ms-transition-duration:0.25s;
+		transition-duration:0.25s;*/
+	}
+	/*h3+p:hover{
+		-webkit-transform:scale(1.03);
+		-moz-transform:scale(1.03);
+		-o-transform:scale(1.03);
+		-ms-transform:scale(1.03);
+		transform:scale(1.03);
+		background:#efefef;
+	}*/
+	@media only screen and(max-width:480px)
+	{
+		h3+p{
+		padding:0.5em 0.5em;
+		margin:0;
+		}
+	}
+</style>
+<!--end of page styles-->
+</head>
+<body>
+
+<!--	<hr class="header-sep" style="padding-top:0; margin-top:0;">
+
+	<hr class="header-sep">
+
+		<nav class="navbar navbar-inverse" role="navigation" style="clear:both;">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button> -->
+		  <!-- <a class="navbar-brand" href="#">Home</a> -->
+		<!--</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+		  <ul class="nav navbar-nav">
+			<li><a href="index">Home</a></li>
+			<li class="dropdown" id="placement">
+			  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Placement<span class="caret"></span></a>
+			  <ul class="dropdown-menu placementsub">
+				<li class="submenu"> <a href="placement-procedure">Procedure</a></li>
+
+				<li class="submenu"><a href="esteemed-recruiters">Recruiters</a></li>
+				<li class="submenu"><a href="includes/files/jnf_nits.docx">Job Notification Form</a></li>
+			  </ul>
+			</li>
+			<li class="dropdown" id="training">
+			  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Training<span class="caret"></span></a>
+			  <ul class="dropdown-menu trainingsub">
+				<li class="submenu"> <a href="internship-policies">Internship Policies</a></li>
+			  </ul>
+			</li>
+			<li class="dropdown" id="academic">
+			  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Academics<span class="caret"></span></a>
+			  <ul class="dropdown-menu academicsub">
+				<li class="submenu"> <a href="academic-life">Academic Life</a></li>
+				<li class="submenu"><a href="student-profile">Student Profile</a></li>
+			  </ul>
+			</li>
+			<li class="active"><a href="why-nits">Why recruit us</a></li>
+			<li><a href="team"> Team </a> </li>
+			<li><a href="facilities"> Facilities </a> </li>
+			<li><a href="reach-us"> Reach us </a> </li>
+		  </ul>
+		 </div>
+		</div>
+	</nav> -->
+
+
+
+
+
+<!-- main content of body here-->
+<div class="col-md-12" role=main>
+	<h2 class="custom" style="margin:0;"><b>VIT Pune - Vishwakarma Institute of Technology</b></h2><hr class="stylish-rule" style="height:2.6px;">
+	<div class="main-content">
+		<h3></b>Nation's Best Talent</h3></b>
+			<p>The students of B.Tech.(IT and ECE) are admitted through AIEEE and represents the top 0.5% of the candidates that appear for the exam nation-wide. This creamy layer of students possess excellent analytical skills, have a thirst for knowledge and are quick learners.
+			Admissions to the M.Tech.programme is made on the basis of GATE scores.</p>
+
+			<h3></b>World Class Curriculum | Faculty</h3></b>
+			<p>The academic curriculum of the institute is one of the best in our country and is designed by academicians and industry experts keeping in mind the dynamically changing world of technology. The institute has had the advantage of having a world class faculty who with their vast knowledge and huge research experience have consistently produced students with strong fundamentals and expertise in their preferred research area.</p>
+
+		  <h3></b>Superlative Thinker | Coders</h3></b>
+			<p>Our students every year take part in renowned programming competitions like ACM-ICPC, Google Summer of Code, Google Code Jam and have always made their presence felt by securing high ranks. We also have a large number of students honing their coding skills on websites like TopCoder, SPOJ, CodeChef etc.</p>
+
+		<h3></b>An Institute of National Importance:</h3></b>
+			<p>With an average annual fund of Rs 150 crores received by the institute, the country envisions its future in us. Identified as a Centre of Excellence by the Government of India, the institute is reinventing itself to crush the competition from its contemporaries in various domains.</p>
+
+		<h3><b>Cutting Edge Research Infrastructure</h3></b>
+			<p>The infrastructure and research facilities of the institute are among the best in the field of IT and ECE. The student here get a hands on experience by working in world class labs with cutting edge equipment. This has led to 11 patents being filed till now and more than 350 research paper are published every year.</p>
+
+		<h3></b>Students with Experience in Industry</h3></b>
+			<p>The institute in collaboration with the government of India, MHRD, Ministry of Science & IT, Ministry of Telecom and other national and international universities, is actively involved in a lot of research projects. The students also take up compulsory research project and go through pre-final year internship to streamline their knowledge with current industry practices.</p>
+
+		<h3></b>Extremely Successful Alumni</h3></b>
+				<p>The alumni of our institute have gone on to become the leaders in their fields and are doing excellent work, both by contributing to their organization’s development and creating their own start-ups. Our Alumni are actively engaged with the institute and the students, motivating them to perform and excel at whatever they do.</p>
+
+		<h3></b>All-Round Development</h3></b>
+				<p>The institute in committed to provide students the opportunity to develop an all rounded personality. The much important soft skills that include communication skills, conflict resolution and, creative problem solving, strategic thinking, team building etc. are inculcated in the students through class-room presentations and open ended learning components.</p>
+
+	</div>
+</div>
+<!--end of content-->
+
+
+
+</body>
+</html>
